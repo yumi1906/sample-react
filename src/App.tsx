7fd021dd22cbd1177fa35b8fe435b776bcdc371e
yumi1906 <yuwasa60@gmail.com>
2023-12-10 09:36:55 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+    const undefined = 'undefined';
+  return (
+      <ul>
+          <li><span>undefined</span>:<span>{undefined}</span></li>
+      </ul>
+  );
+}
