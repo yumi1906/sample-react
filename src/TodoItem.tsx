8dd8b33319578e8fe654fce2badbfe72da55d687
yumi1906 <yuwasa60@gmail.com>
2023-12-10 15:00:28 +0900

    演習3-4

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -7,14 +7,18 @@ type Props = {
 
 export const TodoItem:React.FC<Props> = (props) => {
     const {todoList} = props;
+    const onClick = (task: string) => {
+        console.log(task);
+    };
 
     return (
         <>
             {todoList.map((i) => {
                 return (
-                    <div>
+                    <div key={i.id}>
                         <input type="checkbox" />
                         <label>{i.task}</label>
+                        <button onClick={() =>onClick(i.task)}>delete</button>
                     </div>);
             })}
         </>
