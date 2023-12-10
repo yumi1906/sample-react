b49fd8a6fd8b662c6edeee60b1c434a38898b75c
yumi1906 <yuwasa60@gmail.com>
2023-12-10 10:51:12 +0900

    演習3-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import React from "react";
 import "./styles.css";
 
+const todoList = [
+    {id: 1, task: "Learning Browser", completed: true},
+    {id: 2, task: "Learning JavaScript/TypeScript", completed: true},
+    {id: 3, task: "Learning React", completed: false},
+    {id: 4, task: "Learning Next.js", completed: false},
+];
+
 export default function App() {
-    const sample_string = 'hello';
-    const sample_nuber = 123;
   return (
-      <ul>
-          <li><span>sample_string</span>:<span>{sample_string}</span></li>
-          <li><span>sample_nuber</span>:<span>{sample_nuber}</span></li>
-      </ul>
+      <>
+          {todoList.map((i) =>{
+              return (
+                  <div key={i.id}>
+                      <input type="checkbox" />
+                      <label>{i.task}</label>
+                  </div>
+              );
+          })}
+      </>
   );
 }
